c4bb3da9f18387de1592e0cba69b450f14a76050
Marta Feal <83492877+MartaFeal@users.noreply.github.com>
2022-03-07 13:56:36 +0100

    Update coche.js

--- E9/coche.js
+++ E9/coche.js
@@ -10,8 +10,4 @@ export class Coche {
 }
 
 
-// let Coche1 = new Coche("7,2" + "kg/cv", "8,2" + "cv/l", "Honda Civic");
-// let Coche2 = new Coche("11,7" + "kg/cv", "12,0" + "cv/l", "Opel Corsa");
 
-// console.log(Coche1);
-// console.log(Coche2);
