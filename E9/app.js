ffa49c26e70f66c8c4ca41ee1117f034298c4391
Marta Feal <83492877+MartaFeal@users.noreply.github.com>
2022-03-07 09:40:21 +0100

    Add files via upload

---
+++ E9/app.js
@@ -0,0 +1,6 @@
+import { Coche } from "./coche.js";
+
+let newCoche = new Coche("34,5","35,0", "honda");
+console.log(newCoche);
+
+
---
+++ E9/coche.js
@@ -0,0 +1,17 @@
+
+export class Coche {
+ 
+  constructor(peso, potencia, marca) {
+    this.peso = peso;
+    this.potencia = potencia;
+    this.marca = marca;    
+  };
+  
+}
+
+
+// let Coche1 = new Coche("7,2" + "kg/cv", "8,2" + "cv/l", "Honda Civic");
+// let Coche2 = new Coche("11,7" + "kg/cv", "12,0" + "cv/l", "Opel Corsa");
+
+// console.log(Coche1);
+// console.log(Coche2);
