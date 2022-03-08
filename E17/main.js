9a8d82223c02c4c0aac8f0cc6965615f6e020115
Marta Feal <83492877+MartaFeal@users.noreply.github.com>
2022-03-08 11:48:36 +0100

    Update main.js

--- E17/main.js
+++ E17/main.js
@@ -1,6 +1,5 @@
 import { saludar }  from "./libreria.js";
 import { horaLocal } from "./moment.js";
 
-console.log(saludar('María'));
-console.log(saludar('Javier'));
+console.log(saludar('Alberto'));
 console.log('Son las ' + horaLocal());
