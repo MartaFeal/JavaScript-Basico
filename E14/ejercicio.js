f755bcc5946ce9b1fa7af60e5d6cd60ec0ff4d74
Marta Feal <83492877+MartaFeal@users.noreply.github.com>
2022-03-09 19:52:26 +0100

    Update ejercicio.js

--- E14/ejercicio.js
+++ E14/ejercicio.js
@@ -1,31 +1,19 @@
 
 
-// function sumar() {
-    
-//     let num1 = document.getElementById('num1').value;
-//     let num2 = document.getElementById('num2').value;
-    
-//     let sumar = (num1) + (num2);
-    
-//     document.getElementById('resultado').value = sumar;
-   
-    
-// } 
-
 function sumar(a, b) {
     let resultado = a + b;
     
-    if (a || b == '') {
-        return 'Introduce número';
+     if (typeof a === 'string' || typeof b === 'string') {
+         return 'Introduce número';
     } else {
-        return resultado;
+         return resultado;
     }
 
     
 }
 
 
-let resultado = (sumar(5, 10));
-console.log((resultado));
-let resultado2 = sumar('Javier', 'María');
-console.log(resultado2);
+// let resultado = sumar(5, 10);
+// console.log(resultado);
+// let resultado2 = sumar('Javier', 'María');
+// console.log(resultado2);
